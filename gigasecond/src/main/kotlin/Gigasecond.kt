2c986add2cb1d87df6d8096d5d9396adb4175a31
Wony67 <49256569+Wony67@users.noreply.github.com>
2021-12-18 20:25:38 +0900

    Update gigasecond/src/main/kotlin/Gigasecond.kt
    Co-authored-by: rapidfireworks <rapidfireworks@gmail.com>

--- gigasecond/src/main/kotlin/Gigasecond.kt
+++ gigasecond/src/main/kotlin/Gigasecond.kt
@@ -5,10 +5,7 @@ import java.time.LocalTime
 class Gigasecond {
     val date : LocalDateTime
 
-    constructor(date : LocalDate) {
-        var dateTime = LocalDateTime.of(date, LocalTime.of(0, 0, 0))
-        this.date = dateTime.plusSeconds(1000000000)
-    }
+    constructor(date : LocalDate): this(LocalDateTime.of(date, LocalTime.of(0, 0, 0)))
     constructor(datetime : LocalDateTime) {
         this.date = datetime.plusSeconds(1_000_000_000)
     }
